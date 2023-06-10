a3909cdee820b1853a2ed597160429b9f68f2e9b
Sergey Zubakha <qasergeyzubakha@yandex.ru>
2023-06-10 17:52:03 +0300

    fix and add GetRandoms class, add faker.options().option()

--- src/test/java/com/demoqa/tests/RegistrationPageTestWithRandomData.java
+++ src/test/java/com/demoqa/tests/RegistrationPageTestWithRandomData.java
@@ -4,27 +4,25 @@ import com.demoqa.pages.RegistrationPage;
 import com.github.javafaker.*;
 import org.junit.jupiter.api.Test;
 
-import javax.security.auth.Subject;
-
-import static com.demoqa.pages.RegistrationPage.*;
+import static com.demoqa.utils.GetRandoms.*;
 
 public class RegistrationPageTestWithRandomData extends TestBase {
-    RegistrationPage registrationPage = new RegistrationPage();
-    Faker faker = new Faker();
-    String FirsName = faker.name().firstName(),
-            LastName = faker.name().lastName(),
-            Email = faker.internet().emailAddress(),
-            PhoneNumber = faker.phoneNumber().subscriberNumber(10),
-            Address = faker.address().fullAddress(),
-            Day = String.valueOf(getRandomInt(10, 28)),
-            Month = getRandomMonth(),
-            Year = String.valueOf(getRandomInt(1900, 2100)),
-            Gender = getRandomGender(),
-            Subject = getRandomSubject(),
-            Hobbies = getRandomHobbies(),
-            State = getRandomState(),
-            City = getRandomCity(State),
-            FileName = "qa.png";
+
+    String firstName = faker.name().firstName(),
+            lastName = faker.name().lastName(),
+            email = faker.internet().emailAddress(),
+            phoneNumber = faker.phoneNumber().subscriberNumber(10),
+            address = faker.address().fullAddress(),
+            //day = String.valueOf(getRandomInt(10, 28)),
+            day = String.format("%02d", faker.number().numberBetween(1, 28)),
+            month = getRandomMonth(),
+            year = String.valueOf(getRandomInt(1900, 2100)),
+            gender = getRandomGender(),
+            subject = getRandomSubject(),
+            hobbies = getRandomHobbies(),
+            state = getRandomState(),
+            city = getRandomCity(state),
+            fileName = "qa.png";
 
 
     @Test
@@ -33,32 +31,32 @@ public class RegistrationPageTestWithRandomData extends TestBase {
         //Проверка формы
         registrationPage.openPage()
                 .closeBanners()
-                .setFirsName(FirsName)
-                .setLastName(LastName)
-                .setUserEmail(Email)
-                .setGender(Gender)
-                .setUserNumber(PhoneNumber)
-                .setBirthDay(Day, Month, Year)
-                .setSubject(Subject)
-                .setHobbies(Hobbies)
-                .selectPicture(FileName)
-                .setState(State)
-                .setCity(City)
-                .setAddress(Address)
+                .setFirsName(firstName)
+                .setLastName(lastName)
+                .setUserEmail(email)
+                .setGender(gender)
+                .setUserNumber(phoneNumber)
+                .setBirthDay(day, month, year)
+                .setSubject(subject)
+                .setHobbies(hobbies)
+                .selectPicture(fileName)
+                .setState(state)
+                .setCity(city)
+                .setAddress(address)
                 .submitForm();
 
         //Проверка формы
         registrationPage.verifyGreeting()
-                .verifyResult("Student Name", FirsName + " " + LastName)
-                .verifyResult("Student Email", Email)
-                .verifyResult("Gender", Gender)
-                .verifyResult("Mobile", PhoneNumber)
-                .verifyResult("Date of Birth", Day + " " + Month + "," + Year)
-                .verifyResult("Subjects", Subject)
-                .verifyResult("Hobbies", Hobbies)
-                .verifyResult("Picture", FileName)
-                .verifyResult("Address", Address)
-                .verifyResult("State and City", State + " " + City);
+                .verifyResult("Student Name", firstName + " " + lastName)
+                .verifyResult("Student Email", email)
+                .verifyResult("Gender", gender)
+                .verifyResult("Mobile", phoneNumber)
+                .verifyResult("Date of Birth", day + " " + month + "," + year)
+                .verifyResult("Subjects", subject)
+                .verifyResult("Hobbies", hobbies)
+                .verifyResult("Picture", fileName)
+                .verifyResult("Address", address)
+                .verifyResult("State and City", state + " " + city);
 
         //Закрытие модальной формы
         registrationPage.closeModalForm();
